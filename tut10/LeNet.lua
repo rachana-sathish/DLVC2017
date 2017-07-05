36a0839a1ccbbd456582645471980ebb1955aca6
Rachana Sathish <rachana.sathish@gmail.com>
2017-07-05 13:41:15 +0530

    Add files via upload

---
+++ tut10/LeNet.lua
@@ -0,0 +1,98 @@
+require 'nn'
+require 'cunn'
+require 'torch'
+require 'math'
+require 'optim'
+
+trainset = torch.load('cifar10-train.t7')
+testset = torch.load('cifar10-test.t7')
+classes = {'airplane', 'automobile', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck'}
+
+print(classes[trainset.label[100]])
+
+setmetatable(trainset, 
+    {__index = function(t, i) 
+                    return {t.data[i], t.label[i]} 
+                end}
+);
+trainset.data = trainset.data:double()
+
+function trainset:size() 
+    return self.data:size(1) 
+end
+
+mean = {} -- store the mean, to normalize the test set in the future
+stdv  = {} -- store the standard-deviation for the future
+for i=1,3 do -- over each image channel
+    mean[i] = trainset.data[{ {}, {i}, {}, {}  }]:mean() -- mean estimation
+    print('Channel ' .. i .. ', Mean: ' .. mean[i])
+    trainset.data[{ {}, {i}, {}, {}  }]:add(-mean[i]) -- mean subtraction
+    
+    stdv[i] = trainset.data[{ {}, {i}, {}, {}  }]:std() -- std estimation
+    print('Channel ' .. i .. ', Standard Deviation: ' .. stdv[i])
+    trainset.data[{ {}, {i}, {}, {}  }]:div(stdv[i]) -- std scaling
+end
+
+net = nn.Sequential()
+net:add(nn.SpatialConvolution(3, 6, 5, 5)) -- 3 input image channels, 6 output channels, 5x5 convolution kernel
+net:add(nn.ReLU())                       -- non-linearity 
+net:add(nn.SpatialMaxPooling(2,2,2,2))     -- A max-pooling operation that looks at 2x2 windows and finds the max.
+net:add(nn.SpatialConvolution(6, 16, 5, 5))
+net:add(nn.ReLU())                       -- non-linearity 
+net:add(nn.SpatialMaxPooling(2,2,2,2))
+net:add(nn.View(16*5*5))                    -- reshapes from a 3D tensor of 16x5x5 into 1D tensor of 16*5*5
+net:add(nn.Linear(16*5*5, 120))             -- fully connected layer (matrix multiplication between input and weights)
+net:add(nn.ReLU())                       -- non-linearity 
+net:add(nn.Linear(120, 84))
+net:add(nn.ReLU())                       -- non-linearity 
+net:add(nn.Linear(84, 10))                   -- 10 is the number of outputs of the network (in this case, 10 digits)
+net:add(nn.LogSoftMax())                     -- converts the output to a log-probability. Useful for classification problems
+
+print('Lenet5\n' .. net:__tostring());
+
+criterion = nn.ClassNLLCriterion()
+--[[
+------- Training based on SGD -----------
+trainer = nn.StochasticGradient(net, criterion)
+trainer.learningRate = 0.001
+trainer.maxIteration = 5 -- just do 5 epochs of training.
+
+trainer:train(trainset)
+
+
+---- Test the network
+testset.data = testset.data:double()   -- convert from Byte tensor to Double tensor
+for i=1,3 do -- over each image channel
+    testset.data[{ {}, {i}, {}, {}  }]:add(-mean[i]) -- mean subtraction    
+    testset.data[{ {}, {i}, {}, {}  }]:div(stdv[i]) -- std scaling
+end
+
+correct = 0
+for i=1,10000 do
+    local groundtruth = testset.label[i]
+    local prediction = net:forward(testset.data[i])
+    local confidences, indices = torch.sort(prediction, true)  -- true means sort in descending order
+    if groundtruth == indices[1] then
+        correct = correct + 1
+    end
+end
+
+print(correct, 100*correct/10000 .. ' % ')
+--]]
+
+------------------------------------------------------------
+--------------- Training on GPU ----------------------------
+
+net = net:cuda()
+criterion = criterion:cuda()
+
+trainset.data = trainset.data:cuda()
+trainset.label = trainset.label:cuda()
+
+trainer = nn.StochasticGradient(net, criterion)
+trainer.learningRate = 0.001
+trainer.maxIteration = 5 -- just do 5 epochs of training.
+
+trainer:train(trainset)
+
