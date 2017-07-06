2041984b5432f660f212238d7f486ed08d1ee0be
Rachana Sathish <rachana.sathish@gmail.com>
2017-07-06 20:27:29 +0530

    Delete main_optim.lua

--- tut06/main_optim.lua
+++
@@ -1,5 +0,0 @@
-dofile('3_MSE_SGD.lua')
-dofile('3_MSE_adagrad.lua')
-dofile('3_MSE_adam.lua')
-gnuplot.plot({'SGD',Train_Error[{{},{1}}]},{'Adagrad',Train_Error[{{},{2}}]},{'Adam',Train_Error[{{},{3}}]},{'SGD_valid',Valid_Error[{{},{1}}]},{'Adagrad_valid',Valid_Error[{{},{2}}]},{'Adam_valid',Valid_Error[{{},{3}}]})
---gnuplot.plot({'SGD',Valid_Error[{{},{1}}]},{'Adagrad',Valid_Error[{{},{2}}]},{'Adam',Valid_Error[{{},{3}}]})
